4202eb5392d630587805ac87ba23328725382f5f
Bryan Navarro <diazbnavarro@gmail.com>
2025-10-23 17:55:32 +0000

    finished list, still button and fetch

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,18 +7,41 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 
-	const [task, setTask] = ('')
-	const [list, setList] = ([])
+	const [task, setTask] = useState('')
+	const [list, setList] = useState([])
 
+	const getTask = (event) => {
+		setTask(event.target.value);
+	}
+	const addtoList=(event)=>{
+		if (event.key==='Enter') {
+			if (task!=='') {
+				setList([...list, task]);
+				setTask('');				
+			}
+			
+		}
+	}
 
 	return (
-		<div className="text-center d-flex justify-content-evenly align-items-center mt-4">           
-			<p className='p-4'>
-				<img src={rigoImage} />
-			</p>
-			<div className="flex-fill p-4 fs-1">
-				<label for="yourTask" className="form-label">What needs to be done?</label>
-				<input type="text" className="form-control" id="yourTask" placeholder="Your tasks here"></input>
+		<div>
+			<div className="text-center d-flex justify-content-evenly align-items-center mt-4">
+				<p className='p-4'>
+					<img src={rigoImage} />
+				</p>
+				<div className="flex-fill p-4 fs-1">
+					<label for="yourTask" className="form-label">What needs to be done?</label>
+					<input type="text" className="form-control" id="yourTask" value={task} onKeyDown={addtoList} onChange={getTask} placeholder="Your tasks here"></input>
+				</div>
+			</div>
+			<div className="card m-4 p-4" style={{ width: "18rem" }}>
+				<ul className="list-group list-group-flush">
+					{list.map((lis, index)=>(
+						<li className="list-group-item" key={index}>
+							<span>{lis}</span>
+						</li>
+					))}
+				</ul>
 			</div>
 		</div>
 	);
