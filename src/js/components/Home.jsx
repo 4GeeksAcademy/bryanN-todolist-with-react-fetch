b400759b132701a5edbaeec16149f5a9d90834a3
Bryan Navarro <diazbnavarro@gmail.com>
2025-10-24 15:51:06 +0000

    clear task button created, still to ad fetch-API

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -13,14 +13,27 @@ const Home = () => {
 	const getTask = (event) => {
 		setTask(event.target.value);
 	}
-	const addtoList=(event)=>{
-		if (event.key==='Enter') {
-			if (task!=='') {
+	const addtoList = (event) => {
+		if (event.key === 'Enter') {
+			if (task !== '') {
 				setList([...list, task]);
-				setTask('');				
+				setTask('');
 			}
-			
+
 		}
+	}
+	const deleteTask = (tasktodelete) => {
+		setList(list.filter((_, i) => i !== tasktodelete))
+	}
+	const tasksCounter = () => {
+		if (list.length === 0) {
+			return ('No Tasks')
+		} else if (list.length === 1) {
+			return ('1 Task')
+		} return (list.length + 'Tasks')
+	}
+	const clearTasks =()=>{
+		setList([])
 	}
 
 	return (
@@ -34,14 +47,19 @@ const Home = () => {
 					<input type="text" className="form-control" id="yourTask" value={task} onKeyDown={addtoList} onChange={getTask} placeholder="Your tasks here"></input>
 				</div>
 			</div>
-			<div className="card m-4 p-4" style={{ width: "18rem" }}>
+			<div className="card m-4" style={{ width: "18rem" }}>
 				<ul className="list-group list-group-flush">
-					{list.map((lis, index)=>(
-						<li className="list-group-item" key={index}>
+					{list.map((lis, index) => (
+						<li className="list-group-item d-flex justify-content-between hide-button" key={index}>
 							<span>{lis}</span>
+							<button type="button" className="btn-close close-button" onClick={() => deleteTask(index)} aria-label="Close"></button>
 						</li>
 					))}
 				</ul>
+				<div className="ms-3 d-flex align-items-center justify-content-between">
+					<div>{tasksCounter()}</div>
+					<button type="button" onClick={()=>clearTasks()} className="btn btn-outline-warning">Clear Tasks</button>
+				</div>
 			</div>
 		</div>
 	);
